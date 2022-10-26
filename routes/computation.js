fc04508a924f8e18847023c61416ac57cdb003db
Anu <111705995+Madagoni123@users.noreply.github.com>
2022-10-26 16:06:53 -0500

    computation commited

--- routes/computation.js
+++ routes/computation.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     var x=Math.random();
     var y;
@@ -10,15 +9,15 @@ router.get('/', function(req, res, next) {
     if(y==undefined){
         y=x;
     }
-  res.render('computation', {title: 'f(${y}) is  ${Math.log(y)}' })
+  res.render('computation', {title: `f(${y}) is  ${Math.log(y)}` })
 })
 
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-    res.render('computation',{title: 'f(${y}) is  ${Math.log(y)}'})
+    res.render('computation',{title: `f(${y}) is  ${Math.log(y)}`})
 
-    res.end();
+res.end();
 });
 
 module.exports = router;
